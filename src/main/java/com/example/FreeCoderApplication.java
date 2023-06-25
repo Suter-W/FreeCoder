24d0128221ddc3881caf3a3856d934a565f040f3
Suter <957705376@qq.com>
2023-06-25 16:50:35 +0800

    Initial commit

---
+++ src/main/java/com/example/FreeCoderApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeCoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeCoderApplication.class, args);
+	}
+
+}
