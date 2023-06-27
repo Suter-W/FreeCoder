26bbbe726e8de5e4c5dd8828f068d0fc6c792b0b
Suter <957705376@qq.com>
2023-06-27 10:36:07 +0800

    some comment

--- src/main/java/com/example/controller/adminOrder.java
+++ src/main/java/com/example/controller/adminOrder.java
@@ -17,39 +17,7 @@ public class adminOrder {
     @Autowired
     private adminOrderService adminOrderService;
 
-    @GetMapping("/getTableList")
-    public Result tableList(@RequestParam String restID){
-
-        List<Table> tableList = adminOrderService.tableList(restID);
-        System.out.println(tableList);
-        return Result.success(tableList);
-    }
-    @PostMapping("/addTable")
-    public Result addTable(@RequestBody Table table){
-        adminOrderService.addTable(table);
-        return(Result.success()) ;
-    }
-    @PostMapping("/editTable")
-    public Result editTable(@RequestBody Table table,@RequestParam Integer tableID){
-        String tableName = table.getTableName();
-        Integer tableLimit = table.getTableLimit();
-        String tableType = table.getTableType();
-        adminOrderService.editTable(tableName,tableType,tableLimit,tableID);
-        return(Result.success());
-    }
-
-    @GetMapping("/getTableInfo")
-    public Result getTableInfo(@RequestParam String restID,@RequestParam String tableName,@RequestParam String tableType){
-        Table table = adminOrderService.getTableInfo(restID,tableName,tableType);
-        return(Result.success(table));
-    }
-
-    @DeleteMapping("/deleteTable")
-    public Result deleteTable(@RequestParam Integer tableID){
-        adminOrderService.deleteTable(tableID);
-        return Result.success();
-    }
-
+    //下面两个gettableID与getOrderingID方法用于内部，作用为提取对应的tableID和正在进行的订单ID
     public Integer gettableID(String restID,String tableName,String tableType){
         Integer tableID = adminOrderService.getTableID(restID,tableName,tableType);
         return (tableID);
@@ -60,6 +28,48 @@ public class adminOrder {
         return orderID;
     }
 
+    //tableList为点餐页面开始罗列桌所用，提取该restID下的所有桌
+    @GetMapping("/getTableList")
+    public Result tableList(@RequestParam String restID){
+
+        List<Table> tableList = adminOrderService.tableList(restID);
+        System.out.println(tableList);
+        return Result.success(tableList);
+    }
+    //addTable为添加桌
+    @PostMapping("/addTable")
+    public Result addTable(@RequestBody Table table){
+        adminOrderService.addTable(table);
+        return(Result.success()) ;
+    }
+
+    //editTable为编辑桌
+    @PostMapping("/editTable")
+    public Result editTable(@RequestBody Table table,@RequestParam String restID0,@RequestParam String tableName0,@RequestParam String tableType0){
+        Integer tableID = gettableID(restID0,tableName0,tableType0);
+        String tableName = table.getTableName();
+        Integer tableLimit = table.getTableLimit();
+        String tableType = table.getTableType();
+        adminOrderService.editTable(tableName,tableType,tableLimit,tableID);
+        return(Result.success());
+    }
+    //getTableInfo为展示当前桌信息
+    @GetMapping("/getTableInfo")
+    public Result getTableInfo(@RequestParam String restID,@RequestParam String tableName,@RequestParam String tableType){
+        Table table = adminOrderService.getTableInfo(restID,tableName,tableType);
+        return(Result.success(table));
+    }
+
+    //删除桌
+    @DeleteMapping("/deleteTable")
+    public Result deleteTable(@RequestParam String restID,@RequestParam String tableName,@RequestParam String tableType){
+        Integer tableID = gettableID(restID,tableName,tableType);
+        adminOrderService.deleteTable(tableID);
+        return Result.success();
+    }
+
+
+    //取出订单信息
     @PostMapping("/getOrderInfo")
     public Result getOrderInfo(@RequestParam String restID,@RequestParam String tableName,@RequestParam String tableType){
         Integer tableID = gettableID(restID,tableName,tableType);
@@ -67,7 +77,7 @@ public class adminOrder {
         Order orderInfo = adminOrderService.getOrderInfo(orderID);
         return Result.success(orderInfo);
     }
-
+    //取出订单具体项
     @PostMapping("/getOrderItem")
     public Result getOrderItem(@RequestParam String restID,@RequestParam String tableName,@RequestParam String tableType){
         Integer tableID = gettableID(restID,tableName,tableType);
