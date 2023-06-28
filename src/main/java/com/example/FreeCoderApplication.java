a9f165ffd8d5e8c53305bf52a2f88b6b8179490e
武建鹏 <wu2494547978@163.com>
2023-06-28 17:54:59 +0800

    login_filter

--- src/main/java/com/example/FreeCoderApplication.java
+++ src/main/java/com/example/FreeCoderApplication.java
@@ -2,8 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan //开启对servlet组件支持
 public class FreeCoderApplication {
 
     public static void main(String[] args) {
