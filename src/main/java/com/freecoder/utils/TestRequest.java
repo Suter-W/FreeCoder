bea826307ea37fdf91baf0083340eb5fa788775c
武建鹏 <wu2494547978@163.com>
2023-07-03 19:56:09 +0800

    实现密码模块： 功能实现 后端使用utf8解码 使用rsa私钥解密
    测试函数实现：
    使用utf8编码
    使用rsa公钥加密
    使用java.net 进行request

---
+++ src/main/java/com/freecoder/utils/TestRequest.java
@@ -0,0 +1,24 @@
+package com.freecoder.utils;
+
+import kong.unirest.Unirest;
+
+import java.net.http.HttpResponse;
+
+/**
+ * @ClassName TestRequest
+ * @Description TODO
+ * @DATE 2023/7/3 17:19
+ */
+public class TestRequest {
+    public static void testRequest(String host, String url, String auth, String requestBody) {
+//        HttpResponse<String> response = (HttpResponse<String>) Unirest.post(host+url)
+//                .header("User-Agent", "Apifox/1.0.0 (https://apifox.com)")
+//                .header("Content-Type", "application/json")
+//                .header("Authorization", auth)
+//                .header("Accept", "*/*")
+//                .header("Host", host)
+//                .header("Connection", "keep-alive")
+//                .body(requestBody)
+//                .asString();
+    }
+}
