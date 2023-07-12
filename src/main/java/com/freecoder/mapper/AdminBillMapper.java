dd5fb57b4676c50ce266f5252204ec9a7d53624d
Suter <957705376@qq.com>
2023-07-12 09:48:45 +0800

    一些Order界面额外方法的实现

--- src/main/java/com/freecoder/mapper/AdminBillMapper.java
+++ src/main/java/com/freecoder/mapper/AdminBillMapper.java
@@ -18,4 +18,10 @@ public interface AdminBillMapper {
     List<OrderItem> getHistoricalOrderDetails(String restID, Integer orderID);
 
     List<Order> getHistoricalBill(Integer page, String restID,LocalDate begin, LocalDate end);
+
+    @Select("select * from order_info where orderID = #{orderID}")
+    Order getOrderInfoByid(Integer orderID);
+
+    @Select("select * from order_item where orderID = #{orderID}")
+    List<OrderItem> getOrderItemByid(Integer orderID);
 }
